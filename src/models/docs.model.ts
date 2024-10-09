9f85a46079307aa011539d763645bee9b38a8ff6
Alexandre L Moreira <alexandre_lopees@hotmail.com>
2024-10-09 17:15:03 -0300

    doc: storage interface & dao + updates

--- src/models/docs.model.ts
+++ src/models/docs.model.ts
@@ -1,37 +1,35 @@
 
 interface DocFields {
-  id: number|null
-  name: string|null
-  owner: number|null
-  status: number|null
-  createdAt: number|null
+  id: number
+  name: string
+  owner: number
+  status: number
+  createdAt: number
   updatedAt: number|null
-  deletedAt: number|null
 }
 
 export interface Doc {
   SetName(v: string) : void
   SetStatus(v: number) : void
 
-  Id() : number|null
-  Name() : string|null
-  Status() : number|null
-  CreatedAt() : Date|null
+  Id() : number
+  Name() : string
+  Owner() : number
+  Status() : number
+  CreatedAt() : Date
   UpdatedAt() : Date|null
-  DeletedAt() : Date|null
 }
 
 class DocImpl implements Doc {
   protected props: DocFields = {
-    id: null,
-    name: null,
-    owner: null,
-    status: null,
-    createdAt: null,
+    id: 0,
+    name: "",
+    owner: 0,
+    status: 0,
+    createdAt: 0,
     updatedAt: null,
-    deletedAt: null,
   }
-  constructor(props: {id?: number, name?: string, owner?: number, status?: number, createdAt?: number|null, updatedAt?: number|null, deletedAt?: number|null}){
+  constructor(props: {id?: number, name?: string, owner?: number, status?: number, createdAt?: number|null, updatedAt?: number|null}){
     Object.assign(this.props, props)
   }
   SetId(value: number) {
@@ -52,30 +50,24 @@ class DocImpl implements Doc {
   SetUpdatedAt(value: Date) {
     this.props.updatedAt = value.getTime();
   }
-  SetDeletedAt(value: Date) {
-    this.props.deletedAt = value.getTime();
-  }
   
-  Id(): number|null {
+  Id(): number {
     return this.props.id;
   }
-  Name(): string|null {
+  Name(): string {
     return this.props.name;
   }
-  Owner(): number|null {
+  Owner(): number {
     return this.props.status;
   }
-  Status(): number|null {
+  Status(): number {
     return this.props.status;
   }
-  CreatedAt(): Date|null {
-    return this.props.createdAt === null ? null : new Date(this.props.createdAt);
+  CreatedAt(): Date {
+    return new Date(this.props.createdAt);
   }
   UpdatedAt(): Date|null {
     return this.props.updatedAt === null ? null : new Date(this.props.updatedAt);
-  }
-  DeletedAt(): Date|null {
-    return this.props.deletedAt === null ? null : new Date(this.props.deletedAt);
   }
 }
 
@@ -86,3 +78,177 @@ class DocFactoryImpl {
 }
 
 export const DocFactory = new DocFactoryImpl
+
+export interface DocStorage {
+  CreateDoc(name: string, owner: number, status: number) : Promise<{
+    id: number;
+    name: string;
+    owner: number;
+    status: number;
+    createdAt: Date;
+    updatedAt?: Date|null;
+  }>
+  UpdateDoc(id: number, set: {name?: string, status?: number}) : Promise<{
+    id?: number;
+    name: string;
+    owner: number;
+    status: number;
+    createdAt?: Date;
+    updatedAt: Date;
+  }>
+  DeleteDoc(id: number) : Promise<void>
+  SelectDoc(id: number) : Promise<{
+    id: number;
+    name: string;
+    owner: number;
+    status: number;
+    createdAt: Date;
+    updatedAt?: Date|null;
+  }|null>
+  SelectDocs(options?: {
+    email?: {
+      prefix?: string,
+      infix?: string,
+      suffix?: string
+    }, 
+    name?: {
+      prefix?: string,
+      infix?: string,
+      suffix?: string
+    }
+  }) : Promise<{
+    id: number;
+    name: string;
+    owner: number;
+    status: number;
+    createdAt: Date;
+    updatedAt?: Date|null;
+  }[]>
+}
+
+export const ErrEmailAlreadyExists = new Error("email already exists")
+export const ErrInexistentDoc = new Error("there is no such doc")
+export const ErrUnableToCreateDoc = new Error("unable to create doc due to storage error")
+export const ErrUnableToUpdateDoc = new Error("unable to update doc due to storage error")
+export const ErrUnableToDeleteDoc = new Error("unable to delete doc due to storage error")
+export const ErrUnableToSelectDoc = new Error("unable to delete doc due to storage error")
+
+export interface DocDAO {
+  Create(doc: Doc) : Promise<void>
+  Update(doc: Doc, set: {name?: string, status?: number}) : Promise<void>
+  Delete(id: number) : Promise<void>
+  Select(id: number) : Promise<Doc|null>
+  SelectAll(options?: {
+    name?: {
+      prefix?: string,
+      infix?: string,
+      suffix?: string
+    }
+    status?: number
+  }) : Promise<Doc[]>
+}
+
+class DocDAOImpl implements DocDAO {
+  private sto: DocStorage
+  constructor(sto: DocStorage) {
+    this.sto = sto
+  }
+  async Create(doc: Doc) : Promise<void> {
+    if (!(doc instanceof DocImpl)) {
+      throw new Error("incompatible type")
+    }
+    return new Promise((resolve, reject) => {
+      this.sto.CreateDoc(doc.Name(), doc.Owner(), doc.Status())
+      .then(res => {
+        doc.SetId(res.id)
+        doc.SetCreatedAt(res.createdAt)
+        resolve()
+      })
+      .catch(err => {
+        reject(err)
+      })
+    })
+  }
+  Update(doc: Doc, set: {name?: string, status?: number}): Promise<void> {
+    if (!(doc instanceof DocImpl)) {
+      throw new Error("incompatible type")
+    }
+    return new Promise((resolve, reject) => {
+      this.sto.UpdateDoc(doc.Id(), set)
+      .then(res => {
+        doc.SetName(res.name)
+        doc.SetStatus(res.status)
+        doc.SetUpdatedAt(res.updatedAt)
+        resolve()
+      })
+      .catch(err => {
+        reject(err)
+      })
+    })
+  }
+  Delete(id: number): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.sto.DeleteDoc(id)
+      .then(() => {
+        resolve()
+      })
+      .catch(err => {
+        reject(err)
+      })
+    })
+  }
+  Select(id: number): Promise<Doc | null> {
+    return new Promise((resolve, reject) => {
+      this.sto.SelectDoc(id)
+      .then(res => {
+        let doc = (res === null)?
+          null
+          :
+          new DocImpl({
+            ...res,
+            createdAt: res.createdAt.getTime(),
+            updatedAt: res.updatedAt?.getTime()||null
+          })
+        resolve(doc)
+      })
+      .catch(err => {
+        reject(err)
+      })
+    })
+  }
+  SelectAll(options?: { 
+    name?: {
+      prefix?: string,
+      infix?: string,
+      suffix?: string
+    },
+    status?: number
+  }): Promise<Doc[]> {
+    return new Promise((resolve, reject) => {
+      this.sto.SelectDocs()
+      .then(res => {
+        let docs: Doc[] = res.map(doc => new DocImpl({
+          id: doc.id,
+          name: doc.name,
+          owner: doc.owner,
+          status: doc.status,
+          createdAt: doc.createdAt.getTime(),
+          updatedAt: doc.updatedAt == null ? null : doc.updatedAt.getTime()
+        }))
+        resolve(docs)
+      })
+      .catch(err => {
+        reject(err)
+      })
+    })
+  }
+}
+
+var docDao : DocDAO|null = null
+
+export function CreateDocDAO(storage: DocStorage) : DocDAO {
+  if (docDao !== null) {
+    throw new Error("doc dao has already been setted")
+  }
+  return docDao = new DocDAOImpl(storage);
+}
